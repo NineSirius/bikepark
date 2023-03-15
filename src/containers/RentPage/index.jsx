2031943e379c200e65421c8000922630f09443c2
Ruslan Bulah <ruslanbulah3@gmail.com>
2023-03-15 12:22:19 +0600

    edit main page

--- src/containers/RentPage/index.jsx
+++ src/containers/RentPage/index.jsx
@@ -1,11 +1,31 @@
+import Button from '@/components/UI/Button'
 import styles from './RentPage.module.css'
 
 export const RentPage = () => {
     return (
         <div className="container">
             <div className={styles['card-main-wrap']}>
-                <div className="card-main"></div>
-                <div className="card-main"></div>
+                <div className="card-main">
+                    <h1 className="title">Аренда велосипедов c доставкой</h1>
+
+                    <div className={styles['header-banner']}>
+                        <div>
+                            <div className={styles['header-bunner-item']}>
+                                <span className={styles.price}>Бесплатно</span>
+                            </div>
+                        </div>
+                        <div>
+                            <div className={styles['header-bunner-item']}>
+                                <span className={styles.price}>Бесплатно</span>
+                            </div>
+                        </div>
+                        <div>
+                            <div className={styles['header-bunner-item']}>
+                                <span className={styles.price}>Бесплатно</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div className="card-main"></div>
             </div>
         </div>
