8560991dc5e47108c0613c437c955fae94736cd1
baeil <best0777860106@gmail.com>
2023-03-28 09:36:37 +0600

    ya lox

--- src/containers/RentPage/index.jsx
+++ src/containers/RentPage/index.jsx
@@ -1,10 +1,11 @@
+import { RentBikeCard } from '@/components/RentBikeCard/RentBikeCard'
 import styles from './RentPage.module.css'
 
 export const RentPage = () => {
     return (
         <div className="container">
             <div className={styles['card-main-wrap']}>
-                <div className="card-main"></div>
+                <div className="card-main"> <RentBikeCard title="Аллюминий" price="90 AED" image="/img/bike.png"/> </div>
                 <div className="card-main"></div>
                 <div className="card-main"></div>
             </div>
