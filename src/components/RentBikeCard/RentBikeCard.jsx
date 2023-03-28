c1340f786d022b30728a73282898de1b417871ef
baeil <best0777860106@gmail.com>
2023-03-28 11:16:10 +0600

    rustam lox

--- src/components/RentBikeCard/RentBikeCard.jsx
+++ src/components/RentBikeCard/RentBikeCard.jsx
@@ -2,12 +2,14 @@ import Image from 'next/image'
 import { CheckBox } from '../UI/Checkbox'
 import { Tooltip } from '../UI/Tooltip'
 import styles from './RentBikeCard.module.css'
+import { useState } from 'react'
 
 export const RentBikeCard = ({ title, image, price }) => {
+  const [typeInfo, setTypeInfo] = useState(false)
     return (
         <div>
-            <div className="CardBlock">
-                <Image
+            <div className={styles.cardBlock}>
+                <Image  
                     className={styles.bike}
                     src={image}
                     width={300}
@@ -16,19 +18,21 @@ export const RentBikeCard = ({ title, image, price }) => {
                 />
                 <div className={styles.textcard}>
                     <h2 className={styles.textinfo}>{title}</h2>
-                    <p className={styles.textprice}>{price}</p>
-                    <div className={styles.icons}>
-                        <Tooltip
-                            icon="question"
-                            type="default"
-                            isVisible="{typeInfo}"
-                            setIsVisible="{setTypeInfo}"
-                        >
-                            Легкие, надежные и легко управляются. Возможны
-                            вибрации из-за неровностей дороги.
-                        </Tooltip>
-
-                        <CheckBox type="default" />
+                    <div className={styles['card-info']}>
+                    <p className={styles.textprice}>{price} AED</p>
+                       <div className={styles["card-controls"]}>
+                          <Tooltip
+                              icon="question"
+                              type="default"
+                              isVisible={typeInfo}
+                              setIsVisible={setTypeInfo}
+                          >
+                              Легкие, надежные и легко управляются. Возможны
+                              вибрации из-за неровностей дороги.
+                          </Tooltip>
+  
+                          <CheckBox type="default" />
+                       </div>
                     </div>
                 </div>
             </div>
