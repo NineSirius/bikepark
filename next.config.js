175780310303119401f7787613de4851b3b7069e
Ruslan Bulah <ruslanbulah3@gmail.com>
2023-03-31 17:37:41 +0600

    add statis build files

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: "build",
 };
 
 module.exports = {
   images: {
     domains: ["res.cloudinary.com"],
+    unoptimized: true,
   },
 };
