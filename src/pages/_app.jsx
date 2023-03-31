edc8e8bb2a7751e0048f4111ff38293a80955841
Ruslan Bulah <ruslanbulah3@gmail.com>
2023-03-31 17:11:47 +0600

    add montserrat font

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,21 +2,14 @@ import { Layout } from "@/containers/Layout";
 import "@/styles/globals.css";
 import "@/styles/icons.css";
 import "@/styles/swiper.css";
-import { Montserrat } from "next/font/google";
-
-const montserrat = Montserrat({
-  weight: ["400", "500", "600", "700"],
-  subsets: ["latin"],
-});
+import "@/styles/font.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <div className={montserrat.className}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </div>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   );
 }
